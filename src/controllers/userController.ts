81670c69ac0f6d52e1b4a927c345286b7f3f485b
Mike Misiura <mikemisiura@gmail.com>
2023-07-14 16:06:08 -0600

    user Auth BE finished

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,5 +1,8 @@
 import { RequestHandler } from "express";
 import { User } from "../models/user";
+import { signUserToken } from "../services/auth";
+const Nylas = require('nylas');
+const { default: Draft } = require('nylas/lib/models/draft');
 
 export const loginUser: RequestHandler = async (req, res, next) => {
 
@@ -18,8 +21,33 @@ export const loginUser: RequestHandler = async (req, res, next) => {
 
     // TODO: add authentication
 
-        // let token = await signUserToken(existingUser);
-        res.status(200).json({ existingUser });
+    let token = await signUserToken(existingUser);
+
+    let tokenizedLink = "http://localhost:3001/login/" + token
+
+    console.log(tokenizedLink)
+
+    //Send Email
+    Nylas.config({
+        clientId: "5g6s3fky71a9p1i7kafs9fnd8",
+        clientSecret: "c76lzmje7wme0lbjbuvl2xjfy",
+    });
+
+    const nylas = Nylas.with("aeuRVmDVDEhWFPGjILnDfOLPlQA4a9");
+
+    const draft = new Draft(nylas, {
+        subject: 'User Authentication',
+        body: 'Click this link to login: ' + tokenizedLink,
+        to: [{ name: 'Matthew Slater', email: 'mattslat4@gmail.com' }, { email: existingUser.email }]
+    });
+
+    // Send the draft
+    draft.send().then((message: { id: any; }) => {
+        console.log(`${message.id} was sent`);
+    });
+
+
+    res.status(200).json({ existingUser });
 }
 
 export const createUser: RequestHandler = async (req, res, next) => {
